650ac08f21248e79df17165b136167c28be8dd5f
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-07-26 18:24:16 -0500

    Final still to check non-divisible numbers

--- Non-divisible subset.py
+++ Non-divisible subset.py
@@ -1,15 +1,25 @@
 from itertools import combinations
-n = 4
-k = 7
-S = [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]
-list_=combinations(S, 2)
-list_=list(list_)
-results=[]
-for a in list_:
-    if (a[0]+a[1])%k!=0:
-        results+=[a[0]]
-        results+=[a[1]]
-final_results=len(set(results))
+n = 5
+k = 5
+#S = [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]
+#S=[422346306, 940894801, 696810740, 862741861, 85835055, 313720373]
+S=[2, 7, 12, 17, 22]
+from collections import Counter
+def nonDivisibleSubset(a):
+    c = Counter((i%k for i in a))
+    count = 0
+    blacklist = set()
+    for x,y in c.most_common():
+        if x == k/2 or x==0:
+            count+=1
+        elif k-x not in blacklist:
+            count+=y
+            blacklist.add(x)
+    return count
+
+#n,k = map(int,input().split())
+#a = list(map(int,input().split()))
+print(nonDivisibleSubset(S))
 
 
-print(list_)
+#print(final_results)
