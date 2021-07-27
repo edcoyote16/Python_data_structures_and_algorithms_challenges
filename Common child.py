d848148a924f8b05e9c2b9a2325ce304ceeddcac
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-07-27 00:12:56 -0500

    Final checks

--- Common child.py
+++ Common child.py
@@ -1,6 +1,6 @@
 from collections import Counter
-string1='AA'
-string2='AA'
+string1='WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS'
+string2='FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC'
 string1=[n for n in string1]
 string2=[n for n in string2]
 string1_new=string1[::]
@@ -24,13 +24,13 @@ results=[]
 for index_c, c in enumerate(string1_new):
     for index_d, d in enumerate(string2_new):
         if string1_new[index_c:index_d+1] not in findings1 and string1_new[index_c:index_d+1]!='':
-            findings1+=[len(string1_new[index_c:index_d+1])]
+            findings1+=[string1_new[index_c:index_d+1]]
         if string2_new[index_c:index_d+1] not in findings2 and string2_new[index_c:index_d+1]!='':
-            findings2+= [len(string2_new[index_c:index_d+1])]
+            findings2+= [string2_new[index_c:index_d+1]]
 for e in findings1:
     for f in findings2:
         if e == f:
-            results+=[e]
+            results+=[len(e)]
 
 
          # if string1_new[index_c:index_d]==string2_new[index_c:index_d]:
@@ -42,5 +42,7 @@ for e in findings1:
 #                 findings += [len(string2_new[index_c:index_d])]
 
 
-
-print(max(results))
+if not results:
+    print(0)
+else:
+    print(max(results))
