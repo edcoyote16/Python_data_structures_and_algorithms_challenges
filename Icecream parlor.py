506a2fe1a4ec5f8a915959da1b946efa82b82978
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-07-28 16:57:52 -0500

    Final

--- Icecream parlor.py
+++ Icecream parlor.py
@@ -2,10 +2,12 @@
 
 n = 4
 cost = [2, 2,4, 3]
-results=[]
-for index_a, a in enumerate(cost):
-    for index_b, b in enumerate(cost):
-        if a + b == n and index_a!=index_b:
-            results+=[sorted([index_a+1,index_b+1])]
+def icecreamParlor(n, cost):
+    # Write your code here
+    results=[]
+    for index_a, a in enumerate(cost):
+        for index_b, b in enumerate(cost):
+            if a + b == n and index_a!=index_b:
+                results+=[sorted([index_a+1,index_b+1])]
 
-print(results)
+    return results[0]
