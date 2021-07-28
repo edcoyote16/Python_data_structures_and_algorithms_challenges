42866aa125817e7869eac1c4e2f4cb059fee467a
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-07-28 16:53:42 -0500

    Functionality

--- Icecream parlor.py
+++ Icecream parlor.py
@@ -1,4 +1,11 @@
 
 
-n = 5
-1 4 5 3 2   cost = [1, 4, 5, 3, 2]
+n = 4
+cost = [2, 2,4, 3]
+results=[]
+for index_a, a in enumerate(cost):
+    for index_b, b in enumerate(cost):
+        if a + b == n and index_a!=index_b:
+            results+=[sorted([index_a+1,index_b+1])]
+
+print(results)
