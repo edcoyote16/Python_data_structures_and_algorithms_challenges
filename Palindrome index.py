bc6375ec2c9929f42f8c3f20d7ecdee14ba7f612
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-07-31 13:48:42 -0500

    Underway

---
+++ Palindrome index.py
@@ -0,0 +1,57 @@
+# from collections import Counter
+# #string='qmdpbsswvmqtyhkobqeijjieqbokhytqmvwssbdmq'
+# string='abdccba'
+# length=len(string)
+# odd_value=0
+# counter1=Counter(string)
+# for n in counter1.values():
+#     if n%2==0:
+#         pass
+#     else:
+#         odd_value+=1
+#
+# counter=0
+# results=[]
+# string1=string[:(length+1)//2]
+# string2=string[::-1]
+#
+# for index_a, a in enumerate(string):
+#
+#     b=string2[counter]
+#     if a == b:
+#
+#         counter+=1
+#     else:
+#         results += [counter]
+#         if string2[counter+1]==a:
+#             counter+=2
+#
+#
+#
+#
+#
+#
+# print(a)
+#
+# string='qmdpbsswvmqtyhkobqeijjieqbokhytqmvwssbdmq'
+# half=string[:(len(string)+1)//2]
+# the_other_half=string[(len(string)+1)//2:][::-1]
+# for index_a, a in enumerate(half):
+#     print('')
+#
+#
+#
+# print(half)
+
+string='qmdpbsswvmqtyhkobqeijjieqbokhytqmvwssbdmq'
+string2=[::-1]
+
+
+
+
+
+
+
+
+
+
