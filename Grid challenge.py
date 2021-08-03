6980ec2724a2be920b8b802e525483181906a287
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-08-02 22:08:08 -0500

    Created and OK

---
+++ Grid challenge.py
@@ -0,0 +1,22 @@
+grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
+length=len(grid)
+grid = [sorted(n) for n in grid]
+grid_vertically=[]
+grid_vertically_whole=[]
+for index_a, a in enumerate(range(length)):
+    # if grid_vertically:
+    #     grid_vertically_whole+=[grid_vertically]
+    #     grid_vertically=[]
+    for index_b, b in enumerate(range(len(grid[0]))):
+        grid_vertically+=[grid[b][a]]
+    if grid_vertically:
+        grid_vertically_whole+=[grid_vertically]
+        grid_vertically=[]
+grid_vertically_whole2=grid_vertically_whole[::]
+grid_vertically_whole2=[sorted(n) for n in grid_vertically_whole2]
+if grid_vertically_whole2==grid_vertically_whole:
+    check='Yes'
+else:
+    check='No'
+
+print(check)
