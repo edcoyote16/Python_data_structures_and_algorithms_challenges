034110cad0d809c20f54c186864cb802567a06a4
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-08-02 22:09:33 -0500

    OK

--- Grid challenge.py
+++ Grid challenge.py
@@ -1,22 +1,22 @@
 grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
-length=len(grid)
+length = len(grid)
 grid = [sorted(n) for n in grid]
-grid_vertically=[]
-grid_vertically_whole=[]
+grid_vertically = []
+grid_vertically_whole = []
 for index_a, a in enumerate(range(length)):
     # if grid_vertically:
     #     grid_vertically_whole+=[grid_vertically]
     #     grid_vertically=[]
     for index_b, b in enumerate(range(len(grid[0]))):
-        grid_vertically+=[grid[b][a]]
+        grid_vertically += [grid[b][a]]
     if grid_vertically:
-        grid_vertically_whole+=[grid_vertically]
-        grid_vertically=[]
-grid_vertically_whole2=grid_vertically_whole[::]
-grid_vertically_whole2=[sorted(n) for n in grid_vertically_whole2]
-if grid_vertically_whole2==grid_vertically_whole:
-    check='YES'
+        grid_vertically_whole += [grid_vertically]
+        grid_vertically = []
+grid_vertically_whole2 = grid_vertically_whole[::]
+grid_vertically_whole2 = [sorted(n) for n in grid_vertically_whole2]
+if grid_vertically_whole2 == grid_vertically_whole:
+    check = 'YES'
 else:
-    check='NO'
+    check = 'NO'
 
 print(check)
