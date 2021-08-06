ed3f379c6eeb007e08ea6e8d5ecdc77fff840d6c
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-08-05 22:10:28 -0500

    Final

--- Non generate triangle.py
+++ Non generate triangle.py
@@ -5,10 +5,16 @@ A=[1, 1, 1, 2, 3, 5]
 # A = sorted(int(i) for i in input().split())
 
 i = n-3
+# This loop traverse from right to left, starting at the index -3 because it is
+# checking the sequence of three numbers such that a + b > c and b + c > a and
+# a + c > b
 while i >= 0 and (A[i] + A[i+1] <= A[i+2]) :
+    # then goes to the left one
     i -= 1
-
+# when the condition is met, then it checks the index is at least 0
 if i >= 0 :
+    # and then it prints it
     print(A[i],A[i+1],A[i+2])
 else :
+    # otherwise it prints -1
     print(-1)
