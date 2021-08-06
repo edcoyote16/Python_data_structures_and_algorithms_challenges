a48e9f57192cde4ca663d6006b76419ad3b3fcfd
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-08-05 22:01:35 -0500

    First try

---
+++ Non generate triangle.py
@@ -0,0 +1,14 @@
+n=6
+A=[1, 1, 1, 2, 3, 5]
+
+# n = int(input())
+# A = sorted(int(i) for i in input().split())
+
+i = n-3
+while i >= 0 and (A[i] + A[i+1] <= A[i+2]) :
+    i -= 1
+
+if i >= 0 :
+    print(A[i],A[i+1],A[i+2])
+else :
+    print(-1)
