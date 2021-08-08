beee7505af0a9778baa06705bd16e4ce07af3842
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-08-07 21:23:52 -0500

    Final

--- Game of Thrones - I.py
+++ Game of Thrones - I.py
@@ -1,6 +1,13 @@
 from collections import Counter
 
-s='cdcdcdcdeeeef'
+s='aaabbbb'
 s=Counter(s)
-a=Counter(s)
-print(s)
+#a=Counter(s.values())
+results=[]
+for n in s.values():
+    if n%2!=0:
+        results+=[n%2]
+if not results or len(results)==1:
+    print('YES')
+else:
+    print('NO')
