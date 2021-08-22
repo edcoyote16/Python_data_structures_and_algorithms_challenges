db1947f5ea8c294823e7c9a1b21ca050d875ea99
Edgar_Hernandez <edcoyote16@yahoo.com>
2021-08-21 20:38:24 -0500

    OK, excellent improvements!

--- BST print a node from k distance from a leaf node.py
+++ BST print a node from k distance from a leaf node.py
@@ -1,34 +1,31 @@
-
-
-
-class Node:
-
-    def __init__(self, key):
-        self.key = key
-        self.left = self.right = None
-
+from binarytree import Node
+# class Node:
+#
+#     def __init__(self, key):
+#         self.key = key
+#         self.left = self.right = None
 
 MAX_HEIGHT = 10000
-
 
 def printKDistantfromLeaf(node, k):
     global MAX_HEIGHT
     path = [None] * MAX_HEIGHT
     visited = [False] * MAX_HEIGHT
     # printKDistance(node, k, path, visited, 0)
-    kDistantFromLeafUtil(node, path, visited, 0, k)
+    print(root)
+    results = []
+    kDistantFromLeafUtil(node, path, visited, 0, k, results)
+    return results
 
-    # def printKDistance(node, k, path, visited, pathLen):
-    # Write your Code Here
-
-
-def kDistantFromLeafUtil(node, path, visited, pathLen, k):
+def kDistantFromLeafUtil(node, path, visited, pathLen, k, results):
     # Base case
+
+
     if (node == None):
         return
 
     # append this Node to the path array
-    path[pathLen] = node.key
+    path[pathLen] = node.value
     visited[pathLen] = False
     pathLen += 1
 
@@ -38,27 +35,30 @@ def kDistantFromLeafUtil(node, path, visited, pathLen, k):
     if (node.left == None and node.right == None and
             pathLen - k - 1 >= 0 and
             visited[pathLen - k - 1] == False):
-        print(path[pathLen - k - 1], end=" ")
+        #print(path[pathLen - k - 1], end=" ")
+        results+=[path[pathLen - k - 1]]
         visited[pathLen - k - 1] = True
         return
 
     # If not leaf node, recur for left
     # and right subtrees
     kDistantFromLeafUtil(node.left, path,
-                         visited, pathLen, k)
+                         visited, pathLen, k, results)
     kDistantFromLeafUtil(node.right, path,
-                         visited, pathLen, k)
+                         visited, pathLen, k, results)
+    return results
+
+root = Node(3)
+root.left = Node(8)
+root.right = Node(9)
+root.right.right = Node(3)
+root.left.left = Node(11)
+root.left.right = Node(7)
+root.left.right.left = Node(6)
+root.left.right.right = Node(12)
 
 
 
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
-root.left.left.left = Node(8)
+print(printKDistantfromLeaf(root, 2))
 
-actual = printKDistantfromLeaf(root, 2)
 expected = [1, 2]
